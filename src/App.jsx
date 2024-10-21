ad0be3bfe454009f42b9c85f6c02a01575a93604
AntonioGzlzVzqz <178310997+AntonioGzlzVzqz@users.noreply.github.com>
2024-10-21 13:05:23 -0600

    inicio de proyecto

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { TwitterFollowCard } from './TwitterFollowCard'
+
+export function App() {
+    return(
+        <section className='App'>
+        <TwitterFollowCard userName = "midudev">
+            Miguel Angel Duran
+        </TwitterFollowCard>
+
+        <TwitterFollowCard userName = "pheralb">
+            Pablo Hernandez
+        </TwitterFollowCard>
+        </section>
+    )
+}
